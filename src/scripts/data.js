b8e46c541575c45a62dccde11108015c9e75583e
Igor Yagushevsky <yagushevskij@gmail.com>
2021-02-08 15:31:36 +0300

    code refactoring

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,11 +1,11 @@
 'use strict';
-const API_URL = NODE_ENV === 'production' ? 'https://praktikum.tk' : 'http://praktikum.tk';
+const API_URL = NODE_ENV === 'production' ? 'https://mesto-api.turbomegapro.ru' : 'http://localhost:3001';
 export const config = {
-  cardsApiUrl: `${API_URL}/cohort11/cards`,
-  userApiUrl: `${API_URL}/cohort11/users/me`,
+  cardsApiUrl: `${API_URL}/cards`,
+  userApiUrl: `${API_URL}/users/me`,
   headers: {
     'Content-Type': 'application/json',
-    'authorization': 'a3a931a8-e1c8-4a50-b648-c2aa722fd754'
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDIxMWQ0MDM2MWY1ZjFiMjA1ZTVlNTgiLCJpYXQiOjE2MTI3ODI5MTcsImV4cCI6MTY0NDMxODkxN30._OhqFnyB8_77_EEHVwRJNy7WDw8dIZmYNm5L4LSK8tE'
   },
   userId: 'cd3621b6a6461b94618c14ae',
   paths: {
