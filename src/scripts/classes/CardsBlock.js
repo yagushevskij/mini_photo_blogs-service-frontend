72acdd52d8de7acb55f6b325e3fd38334c8a430f
Igor Yagushevsky <yagushevskij@gmail.com>
2021-04-02 09:25:41 +0300

    fixed cards pagination bug

--- src/scripts/classes/CardsBlock.js
+++ src/scripts/classes/CardsBlock.js
@@ -22,7 +22,7 @@ export class CardsBlock {
   }
 
   _renderCards() {
-    this._sort();
+    // this._sort();
     this._cardsContainer = this._view.querySelector('.cards-list');
     if (this._splittedArray.length > 0) {
       this._renderCardList({
@@ -54,6 +54,7 @@ export class CardsBlock {
       this._splittedArray[i] = this._cardsArr.slice((i * arraySize), (i * arraySize) + arraySize);
     }
     this._totalPages = this._splittedArray.length;
+    // console.log(this._splittedArray)
     this._currentPage = 1;
     if (this._totalPages > this._currentPage) {
       if (this._config.settings.loadBy === 'scroll') {
@@ -69,6 +70,7 @@ export class CardsBlock {
     const containerRelativeBottom = this._container.getBoundingClientRect().bottom;
     const windowHeight = document.documentElement.clientHeight;
     if (containerRelativeBottom < windowHeight + 100) {
+      this._currentPage++;
       if (this._totalPages >= this._currentPage) {
         this._renderCards();
         this._currentPage++;
